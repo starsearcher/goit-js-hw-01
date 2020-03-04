5cf2a656a32829e166aedb3458fff1c13705fca3
starsearcher <57115514+starsearcher@users.noreply.github.com>
2020-03-04 23:39:41 +0200

    v 3.0

--- task-04.js
+++ task-04.js
@@ -7,31 +7,59 @@ console.log(`Дроиды в заказе ${droidQuantity}`);
 let totalPrice;
 let creditsLeft;
 
-droidQuantity = Number(droidQuantity);
-if (droidQuantity > 0) {
-    totalPrice = droidQuantity * pricePerDroid;
-    console.log(`Сумма заказа ${totalPrice}`);
-} else if (droidQuantity <= 0) {
-    console.log(`Пользователь ввел неположительное значение`);
-    alert(`Введите положительное число!`);
-} else if (Number.isNaN(Number(droidQuantity))) {
-    console.log(`Пользователь ввел не число`);
-    alert(`Неверный ввод! Пожалуйста, переоформите заказ.`);
-} else if (droidQuantity === null) {
+if (droidQuantity === null) {
     console.log(`Отменено пользователем!`);
-}
-
-creditsLeft = credits - totalPrice;
-
-if (creditsLeft >= 0) {
-    alert(
-        `Вы купили ${droidQuantity} дроидов, на счету осталось ${creditsLeft} кредитов`,
-    );
-    credits = creditsLeft;
-} else if (creditsLeft < 0) {
-    console.log(`Недостаточно средств на счету!`);
-    alert(`Ваших средств недостаточно`);
-    credits = credits;
+} else {
+    if (droidQuantity <= 0) {
+        console.log(`Пользователь ввел неположительное значение`);
+        alert(`Введите положительное число!`);
+    } else if (Number.isNaN(Number(droidQuantity))) {
+        console.log(`Пользователь ввел не число`);
+        alert(`Неверный ввод! Пожалуйста, переоформите заказ.`);
+    } else if (droidQuantity > 0) {
+        totalPrice = droidQuantity * pricePerDroid;
+        console.log(`Сумма заказа ${totalPrice}`);
+        creditsLeft = credits - totalPrice;
+        if (creditsLeft >= 0) {
+            alert(
+                `Вы купили ${droidQuantity} дроидов, на счету осталось ${creditsLeft} кредитов`,
+            );
+            credits = creditsLeft;
+        } else if (creditsLeft < 0) {
+            console.log(`Недостаточно средств на счету!`);
+            alert(`Ваших средств недостаточно`);
+            credits = credits;
+        }
+    }
 }
 
 console.log(`Баланс по счету ${credits}`);
+
+// droidQuantity = Number(droidQuantity);
+// if (droidQuantity > 0) {
+//     totalPrice = droidQuantity * pricePerDroid;
+//     console.log(`Сумма заказа ${totalPrice}`);
+// } else if (droidQuantity <= 0) {
+//     console.log(`Пользователь ввел неположительное значение`);
+//     alert(`Введите положительное число!`);
+// } else if (Number.isNaN(Number(droidQuantity))) {
+//     console.log(`Пользователь ввел не число`);
+//     alert(`Неверный ввод! Пожалуйста, переоформите заказ.`);
+// } else if (droidQuantity === null) {
+//     console.log(`Отменено пользователем!`);
+// }
+
+// creditsLeft = credits - totalPrice;
+
+// if (creditsLeft >= 0) {
+//     alert(
+//         `Вы купили ${droidQuantity} дроидов, на счету осталось ${creditsLeft} кредитов`,
+//     );
+//     credits = creditsLeft;
+// } else if (creditsLeft < 0) {
+//     console.log(`Недостаточно средств на счету!`);
+//     alert(`Ваших средств недостаточно`);
+//     credits = credits;
+// }
+
+// console.log(`Баланс по счету ${credits}`);
